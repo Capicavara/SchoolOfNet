70fb32dc050c3caca4a1d4004ff5610e7f8d6170
Lucas <lcasmotagomes@hotmail.com>
2018-06-07 21:34:05 -0300

    Initial commit

---
+++ FruitNinja/Assets/Scripts/Movimento.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float velocidade = 10.0f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 movimento = new Vector2(horizontal, 0);
+
+        rb.MovePosition(rb.position + movimento * velocidade * Time.deltaTime);
+	}
+}
